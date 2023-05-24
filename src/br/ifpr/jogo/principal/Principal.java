01687fc4e634dc862b70a84f76f2939b4862aa31
Almeidoka <gustavofederalgg@gmail.com>
2023-05-23 22:32:47 -0300

    criação da primeira janela

---
+++ src/br/ifpr/jogo/principal/Principal.java
@@ -0,0 +1,21 @@
+package br.ifpr.jogo.principal;
+
+import java.security.Permission;
+
+import javax.swing.JFrame;
+
+public class Principal extends JFrame {
+    public Principal() {
+        setVisible(true);
+        setSize(1920, 1080);
+        setTitle("Space Invaders");
+        setLocationRelativeTo((null));
+        setDefaultCloseOperation(ABORT);
+        setBackground("black");
+
+    }
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+    }
+}
